09c1400bc3efe38e00eacdcb7cb55db131cd1339
srivastavravikuamr-lang <srivastavravikuamr@gmail.com>
2026-07-11 12:18:30 +0530

    Create reusable React components

---
+++ frontend/component/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Home(){
+    return(
+       <main className="min-h-screen bg-blue-50 flex flex-col items-center justify-center px-7">
+
+      <h1 className="text-5xl font-bold text-blue-700 mb-4">
+        🩺 MedExplain AI
+      </h1>
+        </main>
+    );
+}
